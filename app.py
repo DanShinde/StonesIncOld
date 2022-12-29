987ebfd89fc122a32f3116f1703a5a42f4dcaedb
DanShinde <112338485+DanShinde@users.noreply.github.com>
2022-12-29 23:29:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
+from time import sleep
 from funs import StepperMotor, create_options, capture_and_save_image, save_Video
-from flask import Flask, render_template, request, redirect, url_for, session
-from flask import send_from_directory
-from pathlib import Path
-import os
+from flask import Flask, render_template, request, redirect, url_for, session, Response
+from PIL import Image
+import os, cv2
 #initial code to for motor
 GPIO_PIN_LIST = [5,6,13,19]
 Motor = StepperMotor(GPIO_PIN_LIST)
@@ -19,11 +19,21 @@ index= 0
 currentFile = 'snap'+str(index)+'.jpeg'
 
 selected_folder =""
+streaming = True
+global img_array
+streaming = True
+recording = False
+global out
+
+img_array = []
 
 
 @app.route("/")
 @app.route("/home",  methods =["GET", "POST"])
 def home():
+    global cap
+    cap = cv2.VideoCapture(0)
+    cap.release()
     FolderName = session.get('selectedFolder')
     options = create_options(app,capturedFolder)
     if request.method == "POST":
@@ -37,6 +47,7 @@ def home():
 def create_folder():
     # Get the selected option from the form
     global selected_folder
+
     selected_option = request.form.get('folder')
     print("New folder ", selected_option)
     # Check if the "create new folder" option was selected
@@ -60,6 +71,8 @@ def create_folder():
 
 @app.route('/start_Photos', methods=["GET", "POST"])
 def start_Photos():
+    global streaming
+    streaming = False
     FolderName = session.get('selectedFolder')
     capture_and_save_image(FolderName)
     return redirect(url_for('home'))
@@ -84,9 +97,102 @@ def take_Video():
     save_Video(FolderName)
     return redirect(url_for('home'))
 
+def gen(rec):
+    # Open the video stream
+    global cap
+    cap = cv2.VideoCapture(0)
+    # Initialize the video writer
+    with app.test_request_context():
+        folder_name = session.get('selectedFolder')
+
+    while True:
+        # Check the status of the streaming
+        if not streaming:
+            cap.release()
+            print("Not streaming")
+            break
+
+        # Read a frame from the video stream
+        success, frame = cap.read()
+        if not success:
+            print("Error reading video stream")
+            break
+        frame = cv2.flip(frame, -1)
+
+        # Encode the frame as JPEG
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        if jpeg is None:
+            print("Error encoding frame as JPEG")
+            break
+        rec = recording
+        # If the recording is on, add the frame to the video
+        if rec:
+            img_array.append(frame)
+            #out.write(frame)
+            print(f"Recording {streaming} {len(img_array)}")
+        #print(streaming, recording, rec)
+        # Yield the JPEG frame
+        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(recording), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/start_stream', methods=['POST'])
+def start_stream():
+  global streaming
+  streaming = True
+  print("Streaming started")
+  return 'Stream started'
+
+@app.route('/stop_stream', methods=['POST'])
+def stop_stream():
+  global streaming
+  streaming = False
+  print("Streaming Stopped")
+  return 'Stream stopped'
+
+@app.route('/rec_on', methods=['POST'])
+def rec_on():
+  global recording
+  recording = True
+  print("Recording Started")
+  Motor.rotate(degrees=360, delay=0.01)
+  # Set the timer for 5 seconds
+  sleep(5)
+  recording = False
+  # Redirect to the rec_stop route to stop the recording
+  return redirect(url_for('rec_off'))
+
+@app.route('/rec_off', methods=["GET",'POST'])
+def rec_off():
+  global recording
+  recording = False
+  FolderName = session.get('selectedFolder')
+  out = cv2.VideoWriter(f"static/StoredData/{FolderName}/exam1.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 24, (640, 480))
+  
+  for i, img in enumerate(img_array):
+    out.write(img)
+    # Construct the file name
+#     file_name = f"image_{i}.jpg"
+#     # Construct the file path
+#     file_path = os.path.join(f"static/StoredData/{FolderName}", file_name)
+#     # Save the image to the file
+#     cv2.imwrite(file_path, img)
+#   image_folder = f"static/StoredData/{FolderName}"
+#   images = [img for img in os.listdir(image_folder) if img.endswith(".jpg") or
+#              img.endswith(".jpeg") or img.endswith("png")]
+#   for image in images:
+#         out.write(cv2.imread(os.path.join(image_folder,image)))
+        #os.remove(os.path.join(image_folder,image))
+  out.release()
+  print(len(img_array))
+  img_array.clear()
+  print("Recording Stopped")
+  return 'Recording stopped'
 
 
-if __name__ =="__main__":\
+if __name__ =="__main__":
     app.run(debug=True)
 
 
